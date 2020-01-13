a78ca5a73cc99528c456a2c1a23f1d9035e46a0c
admin <admin@admins-MacBook-Pro.local>
2020-01-13 15:30:32 -0800

    added all the html with different page.using bootstrap and materialize libraries

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,9 @@
 
-
+  document.addEventListener('DOMContentLoaded', function() {
+    var instance = M.Parallax.getInstance(elem);
+    var elems = document.querySelectorAll('.parallax');
+    var instances = M.Parallax.init(elems, options);
+  });
 
 
 
